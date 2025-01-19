c32fa6afd94c59543e2fb48bcec6d519e718fabd
phaseharry <harrychenhc@icloud.com>
2025-01-19 12:00:27 -0500

    remove spaces from filenames and directories

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Notes for [Learn Concurrent Programming with Go](https://www.manning.com/books/learn-concurrent-programming-with-go)
+
+[Github reference](https://github.com/cutajarj/ConcurrentProgrammingWithGo)
---
+++ chapter-2/2.5-exercises/2.5.3/grepdir.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+func main() {
+	// getting only the filenames from the command
+	searchTerm := os.Args[1]
+	directory := os.Args[2]
+
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		log.Fatalf("Unable to read directory: %v, err: %v", directory, err)
+	}
+
+	for _, file := range files {
+		log.Printf("file: %v, isFile: %v\n", file.Name(), !file.IsDir())
+		if !file.IsDir() {
+			fullPath := filepath.Join(directory, file.Name())
+			go searchFile(searchTerm, fullPath)
+		}
+	}
+
+	time.Sleep(2 * time.Second)
+}
+
+func searchFile(searchTerm string, filename string) {
+	fileContent, err := os.ReadFile(filename)
+	if err != nil {
+		log.Printf("Unable to read file: %v. err: %v\n", filename, err)
+		return
+	}
+
+	stringifiedFileContent := string(fileContent)
+
+	if strings.Contains(stringifiedFileContent, searchTerm) {
+		log.Printf("file: %v contains a match for %v\n", filename, searchTerm)
+	} else {
+		log.Printf("file: %v does not contains a match for %v\n", filename, searchTerm)
+	}
+}
+
+// go run grepdir.go go ~/
