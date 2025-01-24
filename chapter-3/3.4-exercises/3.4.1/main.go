ea97e2135b47f8471c688cd6afafb64e9ccfeacb
phaseharry <harrychenhc@icloud.com>
2025-01-23 19:32:05 -0500

    worked on exercise 3.4.1

---
+++ chapter-3/3.4-exercises/3.4.1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+)
+
+func main() {
+	var wordFrequency = make(map[string]int)
+	for i := 1000; i <= 1030; i++ {
+		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+		go countWordFrequency(wordFrequency, url)
+	}
+
+	time.Sleep(time.Second * 10)
+	for word, count := range wordFrequency {
+		log.Printf("word: %v -> %d\n", word, count)
+	}
+}
+
+func countWordFrequency(wordFrequency map[string]int, url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		panic("Server returning error status code: " + resp.Status)
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+	wordRegex := regexp.MustCompile(`[a-zA-Z]+`)
+
+	words := wordRegex.FindAllString(string(body), -1)
+	for _, w := range words {
+		w = strings.ToLower(w)
+		wordFrequency[w] += 1
+	}
+}
